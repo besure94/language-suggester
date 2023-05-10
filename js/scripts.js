40171d5d0dc7446cd15220f93fae98e6911db69e
Brian Scherner <brianscherner@gmail.com>
2023-05-10 13:20:34 -0700

    Add eventListener for load function. Set variables for form, resetBtn, ruby, csharp and javascript. Add eventListener for 'submit' function and define each id for each radio button.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function() {
+	let form = document.querySelector("form");
+	let resetBtn = document.getElementById("reset");
+	let ruby = document.getElementById("ruby");
+	let csharp = document.getElementById("csharp");
+	let javascript = document.getElementById("javascript");
+});
+
+	form.addEventListener("submit", function(event) {
+		const q1 = document.getElementById("q1").value;
+		const q2 = document.getElementById("q2").value;
+		const q3 = document.getElementById("q3").value;
+		const q4 = document.getElementById("q4").value;
+		const q5 = document.getElementById("q5").value;
+	});
