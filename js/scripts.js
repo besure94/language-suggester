20e3f7860009e1f41135c823d7e15050e3a8b131
Brian Scherner <brianscherner@gmail.com>
2023-05-11 09:51:56 -0700

    Comment out radio button id's and swap in variables using querySelector object obtain values from each radio button.

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,21 @@ window.addEventListener("load", function() {
 	let javascript = document.getElementById("javascript");
 
 	form.addEventListener("submit", function(event) {
-		const qOneYes = document.getElementById("qOneYes").value;
-		const qOneNo = document.getElementById("qOneNo").value;
-		const qTwoYes = document.getElementById("qTwoYes").value;
-		const qTwoNo = document.getElementById("qTwoNo").value;
-		const qThreeYes = document.getElementById("qThreeYes").value;
-		const qThreeNo = document.getElementById("qThreeNo").value;
-		const qFourYes = document.getElementById("qFourYes").value;
-		const qFourNo = document.getElementById("qFourNo").value;
-		const qFiveYes = document.getElementById("qFiveYes").value;
-		const qFiveNo = document.getElementById("qFiveNo").value;
-		const yes = document.querySelector("input[name'operator']:checked").value;
-		const no = document.querySelector("input[name='operator']:checked").value;
+		// const qOneYes = document.getElementById("qOneYes").value;
+		// const qOneNo = document.getElementById("qOneNo").value;
+		// const qTwoYes = document.getElementById("qTwoYes").value;
+		// const qTwoNo = document.getElementById("qTwoNo").value;
+		// const qThreeYes = document.getElementById("qThreeYes").value;
+		// const qThreeNo = document.getElementById("qThreeNo").value;
+		// const qFourYes = document.getElementById("qFourYes").value;
+		// const qFourNo = document.getElementById("qFourNo").value;
+		// const qFiveYes = document.getElementById("qFiveYes").value;
+		// const qFiveNo = document.getElementById("qFiveNo").value;
+		const q1 = document.querySelector("input[name='q1']:checked").value;
+		const q2 = document.querySelector("input[name='q2']:checked").value;
+		const q3 = document.querySelector("input[name='q3']:checked").value;
+		const q4 = document.querySelector("input[name='q4]:checked").value;
+		const q5 = document.querySelector("input[name='q5']:checked").value;
 
 		ruby.setAttribute("class", "hidden");
 		csharp.setAttribute("class", "hidden");
